9841c06bde5c448bd21d75aebbe5213da2efd1c1
Daniel Pichardo <danielpichardo716@gmail.com>
2017-02-05 22:07:37 +0000

    Added hirb gem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,13 @@
 class ContactsController < ApplicationController
     def new
+        # New instance variable using the contact.rb model
         @contact = Contact.new
     end
     
     def create
+        # New instance variable using the form attribute values, defined below
         @contact = Contact.new(contact_params)
+        # .save to database
         if @contact.save
             redirect_to new_contact_path, notice: "Message sent."
         else
@@ -12,6 +15,7 @@ class ContactsController < ApplicationController
         end
     end
     
+    # Get the parameters securely, private will only allow the method to be executed internally
     private
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
