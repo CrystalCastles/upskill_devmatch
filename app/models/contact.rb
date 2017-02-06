266a6963c053ff8e4f1b736a8ed76f9e18a5d2dc
Daniel Pichardo <danielpichardo716@gmail.com>
2017-02-06 21:10:40 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 # Gets attributes from db schema file
 class Contact < ActiveRecord::Base
+    # Form validation
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
